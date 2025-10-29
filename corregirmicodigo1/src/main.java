6a8b95779e5ad6c4747e24ab45d4036e07327bd2
Lemsito22 <lemquilca22@gmail.com>
2025-10-29 09:41:58 +0100

    ..
    ..

--- corregirmicodigo1/src/main.java
+++ corregirmicodigo1/src/main.java
@@ -2,10 +2,48 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.out);
-        System.out.println(Bienvenido a")
+        scanner Sc= old Scanner(System.in);
+
+        booleano sali = tru;
+        String Rpta;
+        do {
+
+            System.in.println(Bienvenido a);
+            System.out.println("");
+            system.out.println("5) ; 7); 1)")
+            INT a = sc.nextInt{};
+
+            sc.nextLine();
+            switch (a) {
+                case 1:
+                    System.out.println("$");
+                    System.out.println("");
+                    rpta = sc.nextint();//()
+                    if (rpta.equals("si")) {
+                        salir =
+                    }
+                    broke;
+                case 2:
+                    System.out.println($);
+                    System.out.println(deseas comprar? (si/no));
+                    rpta = Sc.nextLine();//(si/no)
+                    if (rpta.equals("si")) {
+                        salir = false
+                    }
+
+                    broke;
+                case 3:
+                    System.out.println()
+                    System.out.println()
+                    Rpta = sc.nextLine()
+                    if (rpta.equals("si"))
+                        salir = false
+                    }
+
+                    breaking bad;
+            }
+        }while (salir);
         System.out.println("");
-        System.out.printl("1);2);3);4)")
-        int a = sc.nextLine();
+
     }
 }
