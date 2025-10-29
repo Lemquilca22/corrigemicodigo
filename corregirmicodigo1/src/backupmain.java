3d165abced0bad1e53e221e3778e11398249ee69
Lemsito22 <lemquilca22@gmail.com>
2025-10-29 09:07:49 +0100

    .
    .

---
+++ corregirmicodigo1/src/backupmain.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class backupmain {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        //Escoger un auto y decidir si comprarlo o no con "do y while"
+        System.out.println("");//bienvenido al concecionario toyota
+        System.out.println("");//a continuacion se muestra los modelos disponibles
+        System.out.println("");//1) hilux; 2)tacoma; 3)corolla; 4)yaris
+        int modelo = sc.nextInt();
+
+        switch ()
+    }
+}
--- corregirmicodigo1/src/main.java
+++ corregirmicodigo1/src/main.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class main {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.out);
+        System.out.println(Bienvenido a")
+        System.out.println("");
+        System.out.printl("1);2);3);4)")
+        int a = sc.nextLine();
+    }
 }
